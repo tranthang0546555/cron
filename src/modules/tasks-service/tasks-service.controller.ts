15844e72a3f77d488cb58179f2dfa37046577073
Wyn <65168044+tranthang0546555@users.noreply.github.com>
2024-11-27 04:50:02 +0000

    recommit

--- src/modules/tasks-service/tasks-service.controller.ts
+++ src/modules/tasks-service/tasks-service.controller.ts
@@ -3,12 +3,15 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Controller('tasks-service')
 export class TasksServiceController {
-    private readonly logger = new Logger(TasksServiceController.name);
+  private readonly logger = new Logger(TasksServiceController.name);
 
-    @Cron(CronExpression.EVERY_10_MINUTES)
-    async handleCron() {
-        this.logger.log('Every 10 minutes, avoid shutting down the server');
-        await fetch(process.env.API_TARGET)
-        this.logger.log('Called :: '+process.env.API_TARGET);
-    }
+  @Cron(CronExpression.EVERY_10_MINUTES)
+  async handleCron() {
+    const targets = process.env.API_TARGET.split(',');
+    this.logger.log('Every 10 minutes, avoid shutting down the server');
+    targets.forEach(async (target) => {
+      await fetch(target);
+      this.logger.log('Called :: ' + target);
+    });
+  }
 }
